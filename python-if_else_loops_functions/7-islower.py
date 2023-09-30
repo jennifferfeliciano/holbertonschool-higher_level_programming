943f29d3676634d79ef9bc9f9c779e33ad48e407
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-09-30 01:51:40 +0000

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= chr(97) and c <= chr(122):
-        return True
-    elif c >= chr(65) and c <= chr(90):
+    if len(c) == 0:
+        # Handle the case of an empty string
         return False
+    elif ord('a') <= ord(c) <= ord('z'):
+        return True
     else:
-        return
+        return False
+
