02928909e4f3946a8e0bac0e11701ccab98a86e0
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-09-30 02:31:05 +0000

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if len(c) == 0:
-        # Handle the case of an empty string
-        return False
-    elif ord('a') <= ord(c) <= ord('z'):
+    if ord('a') <= ord(c) <= ord('z'):
         return True
     else:
         return False
