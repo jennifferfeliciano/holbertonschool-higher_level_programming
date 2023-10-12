563d6a5f880a197e16caca311838a9747d8701e6
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-10-12 21:09:42 +0000

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class Square
+"""class that represent a square"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        return self.__size ** 2
