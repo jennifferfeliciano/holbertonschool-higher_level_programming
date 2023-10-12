aedd6c19246b7f995836f103aadf1e762cce6d94
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-10-12 21:27:14 +0000

    task 5 and task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define a class Square that represents a square."""
+
+
+class Square:
+    """A class to represent a square."""
+
+    def __init__(self, size=0):
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using '#' characters and adjusting the position."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self._Square__position[0] + "#" * self.__size)
