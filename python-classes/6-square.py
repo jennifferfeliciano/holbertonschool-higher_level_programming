2eb8c40a3bc67b4d81e886063a2a82d2951fde44
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-10-12 21:36:47 +0000

    fix position property

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,9 +5,9 @@
 class Square:
     """A class to represent a square."""
 
-    def __init__(self, size=0):
-
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -17,12 +17,23 @@ class Square:
     @size.setter
     def size(self, value):
         """Set the size of the square."""
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square."""
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(x, int) for x in value) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         return self.__size ** 2
@@ -35,4 +46,5 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print(" " * self._Square__position[0] + "#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
+
