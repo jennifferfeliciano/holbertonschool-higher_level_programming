afaa80be08e05ce58e65403be6e04a79ace03355
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-10-21 18:41:03 -0400

    add documentation and fix style by PEP*"

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+
+
 def lookup(obj):
+    """function that return a list of attributes and methods"""
     return dir(obj)
--- python-inheritance/1-my_list.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-class Mylist(list):
-    def print_sorted(self):
-        print(sorted)(self)
