e95ff7e7298ab258732042c495e77656e7b025ac
jennifferfeliciano <jenniffer.felicianovelez@gmail.com>
2023-10-22 15:29:38 -0400

    remove redundant if statement

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,4 @@ def is_kind_of_class(obj, a_class):
     or if the object is an instance of a
     class that inherited from, the specified class"""
 
-    if issubclass(type(obj), a_class):
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class)
