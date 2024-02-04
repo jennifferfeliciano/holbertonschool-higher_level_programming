0a25053aadb7635927e70508cc04d5be77a22efe
jennifferfelicianovelez <jennifferfelicianovelez@penguin>
2024-02-04 16:27:48 -0400

    .

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    squared_matrix = [[(i**2) for i in row] for row in matrix]
-    return squared_matrix
+    new_matrix = [[(i**2) for i in row] for row in matrix]
+    return new_matrix
