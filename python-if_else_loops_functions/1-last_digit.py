9c6dea4f2a2871f845d5b409174c3ccc2a4db6ac
jennifferfelicianovelez <jennifferfelicianovelez@penguin>
2024-02-20 18:24:26 -0400

    add - sign when number is less than 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 
 last_dig = abs(number) % 10
+sign = '-' if number < 0 else ''
 
 if last_dig > 5:
-    print(f"Last digit of {number} is {last_dig} and is greater than 5")
+    print("Last digit of {} is {}{} and is greater than 5".format(number, sign, last_dig))
 elif last_dig == 0:
-    print(f"Last digit of {number} is {last_dig} and is 0")
+    print("Last digit of {} is {} and is 0".format(number, last_dig))
 else:
-    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
+    print("Last digit of {} is {}{} and is less than 6 and not 0".format(number, sign, last_dig))
