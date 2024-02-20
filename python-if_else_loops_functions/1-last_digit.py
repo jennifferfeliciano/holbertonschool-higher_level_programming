3b0c4515ba1260596a1be9f55c3bb5621487040f
jennifferfelicianovelez <jennifferfelicianovelez@penguin>
2024-02-20 11:51:14 -0400

    delete an extra variable

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-n = number
-if n:
-    last_dig = abs(n) % 10
-    if last_dig > 5:
-        print(f"Last digit of {n} is {last_dig} and is greater than 5")
-    if last_dig == 0:
-        print(f"Last digit of {n} is {last_dig} and is 0")
-    elif last_dig < 6 and last_dig != 0:
-        print(f"Last digit of {n} is {last_dig} and is less than 6 and not 0")
+
+last_dig = abs(number) % 10
+
+if last_dig > 5:
+    print(f"Last digit of {number} is {last_dig} and is greater than 5")
+elif last_dig == 0:
+    print(f"Last digit of {number} is {last_dig} and is 0")
+else:
+    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
