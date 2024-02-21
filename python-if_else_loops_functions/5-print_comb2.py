b7aec2f6917a069b4a12ea98e649ee9773079718
jennifferfelicianovelez <jennifferfelicianovelez@penguin>
2024-02-21 16:53:21 -0400

    delete newline

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-    print("{:02d}".format(number), end=', ' if number <99 else '\n')
+    print("{:02d}".format(number), end=', ' if number <99 else '')
