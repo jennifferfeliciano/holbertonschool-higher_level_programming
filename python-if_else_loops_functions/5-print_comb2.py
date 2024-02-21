cf2e055b29e4b277dccff4ba3a87301a484031c8
jennifferfelicianovelez <jennifferfelicianovelez@penguin>
2024-02-21 16:52:01 -0400

    fix format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-    if number <= 98:
-        print(f'{number:02d}', end=', ')
-    if number == 99:
-        print(f'{number}')
+    print("{:02d}".format(number), end=', ' if number <99 else '\n')
