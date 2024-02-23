faddc5bd9c5bb7dd7cd0e9676b2a475170f91195
jennifferfelicianovelez <jennifferfelicianovelez@penguin>
2024-02-23 00:33:18 -0400

    fix indentation

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 if __name__ =="__main__":
     args = len(sys.argv) - 1
 
-if args == 0:
-    print("0 arguments.")
-elif args == 1:
-    print("1 argument:")
-else:
-    print(f"{args} arguments:")
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
+    else:
+        print(f"{args} arguments:")
 
-    if args >= 1:
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
+        if args >= 1:
+            for i, arg in enumerate(sys.argv[1:], start=1):
+                print(f"{i}: {arg}")
